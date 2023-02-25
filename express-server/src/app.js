e46e549fc0ff69aedb7c1ad2a9633c6f070d1c7d
tedmcruz <tedcruz@live.com>
2023-02-25 00:21:51 -0400

    ExpressServerV4- code cleanup

--- express-server/src/app.js
+++ express-server/src/app.js
@@ -1,22 +1,39 @@
 import express from "express";
+/* // fs IS USED IN OPTION 2 TO CREATE PRODUCTS VARIABLE 
 import fs from "fs";
+*/ 
 import ProductManager from "./productManager.js";
-// // import products from "./Products.json";
+
+/* // OPTION 1 TO CREATE PRODUCTS VARIABLE = EXTERNAL CODE
 import { createRequire } from "module";
 const require = createRequire(import.meta.url); // construct the require method
-
 const products =  require("./Products.json") // use the require method
+*/
 
-// const products = await fs.promises.readFile("./Products.json","utf-8");
+/* // OPTION 2 TO CREATE PRODUCTS VARIABLE = DIRECT PULL FORM ".JSON" FILE
+const products = JSON.parse(await fs.promises.readFile("./Products.json","utf-8"));
+*/
+
+/* // OPTION 3 TO CREATE PRODUCTS VARIABLE = PULL FROM ".JS" FILE
+const productManager = new ProductManager("./Products.json");
+const products = await productManager.getProducts();
+*/
+
+const productManager = new ProductManager("./Products.json");
+const products = await productManager.getProducts();
+
 const app = express();
 
-// const productManager = new ProductManager();
+/* // CONST PRODUCTMANAGER AND PRODUCTS TP TEST SERVER 
 
-// [
-//     {id:"1",name:"producto1",description:"description",price:"10",thumbnail:"thumbnail",code:"100",stock:"1000"},
-//     {id:"2",name:"producto2",description:"description",price:"20",thumbnail:"thumbnail",code:"200",stock:"2000"},
-//     {id:"3",name:"producto3",description:"description",price:"30",thumbnail:"thumbnail",code:"300",stock:"3000"}
-// ]
+    const productManager = new ProductManager();
+
+    let products = [
+        {id:"1",name:"producto1",description:"description",price:"10",thumbnail:"thumbnail",code:"100",stock:"1000"},
+        {id:"2",name:"producto2",description:"description",price:"20",thumbnail:"thumbnail",code:"200",stock:"2000"},
+        {id:"3",name:"producto3",description:"description",price:"30",thumbnail:"thumbnail",code:"300",stock:"3000"}
+    ]
+*/
 
 app.get("/products", (req,res)=>{
     const {limit} = req.query;
@@ -24,20 +41,24 @@ app.get("/products", (req,res)=>{
         return res.send(products);
     }
     const productsLimit = products.slice(0, limit);
-    // res.send(products.filter((p)=>p.id <= limit));
+    /* // ALTERNATIVE TO LIMIT OPTIONS
+    // res.send(products.filter((p)=>p.id <= limit)); // IF AN ID IS DELETED ON THE MIDDLE, THIS OPTION WILL NOT GIVE THE DESIRED RESULTS.
+    */
     res.send(productsLimit);
-    res.end();
+    // res.end();
 })
 
 app.get("/products/:id", (req,res)=>{
-    const { id } = req.params; // (  const { id } = req.params  ) === (  const id = req.params.id  )
-
+    /* WAYS TO ASSIGN VALUE TO PROPERTIES
+    const { id } = req.params 
+    const id = req.params.id
+    */ 
+   
+    const { id } = req.params;
     let productById = products.find(p => p.id === id);
-    // productById = JSON.stringify(productById);
-    // res.send(`Cliente asked for id = ${id}`)
     console.log(productById);
     res.send(productById);
-    res.end();    
+    // res.end();    
 });
 
 app.listen(8080, () => {
