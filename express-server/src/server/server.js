ff400b7da285cfc208b875494c2478e7ca07381f
tedmcruz <tedcruz@live.com>
2023-03-09 19:01:14 -0400

    Websockets+HandlebarV1 - Starting Setup

--- express-server/src/server/server.js
+++ express-server/src/server/server.js
@@ -1,12 +1,28 @@
 import express from "express";
 import productsManagerRouter from "../routers/productManager.routers.js";
 import cartsManagerRouter from "../routers/cartManager.routers.js";
-// import { dirname } from "path";
+import handlebars from "express-handlebars";
+import __dirname from "./utils.js";
 
 const app = express();
-
 app.use(express.json());
-// app.use(express.static(dirname + "../../public"));
+
+
+app.engine('handlebars',handlebars.engine());
+
+app.set('views',__dirname+"../../views");
+
+app.set('view engine','handlebars');
+
+app.use(express.static(__dirname + "../../public"));
+
+app.get('/',(req,res)=>{
+    let testUser = {
+        name:"Coder",
+        last_name:"House"
+    }
+    res.render('index',testUser)
+})
 
 app.use("/api/products", productsManagerRouter);
 app.use("/api/carts", cartsManagerRouter);
