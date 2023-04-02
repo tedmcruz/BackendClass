e72aa2fe082b478fd258d4a1ada7b7377db1ea3d
tedmcruz <tedcruz@live.com>
2023-04-01 21:46:44 -0400

    ProjectPart2V1 - Project Structure 3

--- express-server/src/routers/productManager.routers.js
+++ express-server/src/routers/productManager.routers.js
@@ -9,13 +9,17 @@ productManagerRouter.use(json());
 const fileConfiguration = dbFileConfiguration.persistenceType;
 
 productManagerRouter.get("/", async (req,res) =>{
+    const {limit} = req.query;
     try{
         if (fileConfiguration === "db"){
             const products = await productModel.find();
-            res.send({result:"success", payload:products});
+            if (!limit){
+                return res.send({result:"success", payload:products});
+            }
+            const productsLimit = products.slice(0, limit);
+            res.send({result:"success", payload:productsLimit});
         } else if (fileConfiguration === "file"){
             const products = await productManager.getProducts();
-            const {limit} = req.query;
             if (!limit){
                 return res.send(products);
             }
@@ -59,16 +63,50 @@ productManagerRouter.put("/:pid", async (req,res)=>{
     const { pid } = req.params;
     let thumbnails = [];
     const {title, description, code, price, stock, category} = req.body;
-    const updateProduct = await productManager.updateProduct(pid, title, description, code, price, stock, category,thumbnails);
-    res.send(updateProduct);
+    const updateProductData = req.body;
+    
+    try{
+        if (fileConfiguration === "db"){
+            if(!title || !description || !code || !price){
+                return res.status(400).send({result:"error",payload:"Missing fields"})
+            } 
+            const updateProduct = await productModel.findOneAndUpdate(
+                {_id: pid},
+                updateProductData,
+                {new: true},
+            )
+            res.status(201).send({result:"success", payload:updateProduct});
+
+        } else if (fileConfiguration === "file"){
+            const updateProduct = await productManager.updateProduct(pid, title, description, code, price, stock, category,thumbnails);
+            res.send(updateProduct);
+        }
+    }catch (e){
+        res.status(500).send({result:"error", payload:e});
+    }
 });
 
 productManagerRouter.delete("/:pid", async (req,res)=>{
     
     const { pid } = req.params;
-    const deleteProduct = await productManager.deleteProduct(pid);
-    let updatedProducts = deleteProduct;
-    res.send(updatedProducts);
+
+    try{
+        if (fileConfiguration === "db"){
+            const {pid} = req.params;
+
+            const deleteProduct = await productModel.deleteOne(
+                { _id: pid}
+            );
+
+            res.status(201).send({result:"success", payload:deleteProduct})
+
+        } else if (fileConfiguration === "file"){
+            const deleteProduct = await productManager.deleteProduct(pid);
+            res.send(deleteProduct);
+        }
+    }catch (e){
+        res.status(500).send({result:"error", payload:e});
+    }
 });
 
 export default productManagerRouter;
