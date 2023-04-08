6c3b029c8fe61ac823ddb86b2927d6163142a18a
tedmcruz <tedcruz@live.com>
2023-04-08 10:47:56 -0400

    ProjectPart2- Fixed Add Product to Cart from single product page

---
+++ express-server/src/public/js/productToCart.js
@@ -0,0 +1,18 @@
+const socket = io();
+
+socket.emit("message","Greetings from products template through websocket");
+
+socket.on("message", (data) => {
+    console.log(data);
+});
+
+const singleProductPageAddToCartButton= document.getElementById("singleProductPageAddToCartButton")
+singleProductPageAddToCartButton.addEventListener("click", (ev) => {       
+    const cartId = document.getElementById("singleProductPageCartId").value;
+    const productId = document.getElementById("singleProductPageProductId").value;
+    const quantity = 1;
+    console.log(cartId);
+    console.log(productId);
+    socket.emit("singleProductPage-addProduct", cartId,productId,quantity)
+}   
+);
