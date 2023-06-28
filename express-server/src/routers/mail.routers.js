908face467d0b4c04974a435023194689cf802f4
tedmcruz <tedcruz@live.com>
2023-06-28 16:47:39 -0400

    ProjectPart5V2 - Implementing new features - Fixing Mailing

--- express-server/src/routers/mail.routers.js
+++ express-server/src/routers/mail.routers.js
@@ -4,10 +4,26 @@ import { authenticateToken } from "../utils.js";
 import nodemailer from "nodemailer";
 
 const router = Router();
+const transport = nodemailer.createTransport({
+    service:"gamil",
+    port:587,
+    auth:{
+        user:"do.not.reply.passcode.reset@gmail.com",
+        pass:"zqozmihizevlyiml"
+    }
+})
 
-router.get("/testEmail",(req,res)=>{
-    res.render("mail",{
-        
+router.get("/testEmail",async(req,res)=>{
+    let result = await transport.sendMail({
+        from:"Coder Tests <do.not.reply.passcode.reset@gmail.com>",
+        to:"tedcruz@live.com",
+        subject:"Test Email",
+        html:`
+        <div>
+            <h1> This is a Test! </h1>
+        </div>
+        `,
+        attachments:[]
     })
 })
 
